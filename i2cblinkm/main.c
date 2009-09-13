35712c3c9d86c6965e83c07c27c5e2656e5f3522
kwilson <kwilson@cs.pdx.edu>
2009-09-13 11:12:54 -0700

    more hacking trying to figure out interrupt issues

--- i2cblinkm/main.c
+++ i2cblinkm/main.c
@@ -2,30 +2,30 @@
 
 /*
  * Portions of this code:
-	FreeRTOS.org V5.1.2 - Copyright (C) 2003-2009 Richard Barry.
+ FreeRTOS.org V5.1.2 - Copyright (C) 2003-2009 Richard Barry.
 
-	This file is part of the FreeRTOS.org distribution.
+ This file is part of the FreeRTOS.org distribution.
 
-	FreeRTOS.org is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+ FreeRTOS.org is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-	FreeRTOS.org is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+ FreeRTOS.org is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with FreeRTOS.org; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU General Public License
+ along with FreeRTOS.org; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-	A special exception to the GPL can be applied should you wish to distribute
-	a combined work that includes FreeRTOS.org, without being obliged to provide
-	the source code for any proprietary components.  See the licensing section 
-	of http://www.FreeRTOS.org for full details of how and when the exception
-	can be applied.
-*/
+ A special exception to the GPL can be applied should you wish to distribute
+ a combined work that includes FreeRTOS.org, without being obliged to provide
+ the source code for any proprietary components.  See the licensing section 
+ of http://www.FreeRTOS.org for full details of how and when the exception
+ can be applied.
+ */
 
 /* Scheduler includes. */
 #include "FreeRTOS.h"
@@ -103,86 +103,77 @@
 #define mainMAM_TIM_3		( ( unsigned portCHAR ) 0x03 )
 #define mainMAM_MODE_FULL	( ( unsigned portCHAR ) 0x02 )
 
-
-
 #define PCLK    configCPU_CLOCK_HZ
 
-
-
-
 uint32_t microsecondsToCPUTicks(const uint32_t microseconds) {
-	uint32_t ret = (configCPU_CLOCK_HZ / 1000000) * microseconds;
-	return(ret);
+    uint32_t ret = (configCPU_CLOCK_HZ / 1000000) * microseconds;
+    return(ret);
 }
 
 uint32_t milisecondsToCPUTicks(const uint32_t miliseconds) {
-	uint32_t ret = (configCPU_CLOCK_HZ / 1000) * miliseconds;
-	return(ret);
+    uint32_t ret = (configCPU_CLOCK_HZ / 1000) * miliseconds;
+    return(ret);
 }
-
 
 #define BLINKM_ADDR 0x09
 
 static void i2cblinkmTask(void *pvParameters) {
-	int x = 0;
-	signed portCHAR theChar;
-	signed portBASE_TYPE status;
-	const int interval = 100000;
-	
-	uint32_t blinkm_id;
+    int x = 0;
+    signed portCHAR theChar;
+    signed portBASE_TYPE status;
+    const int interval = 100000;
 
-	uint32_t pwmDutyCycle = 1000;
+    uint32_t blinkm_id;
 
-        int myDataToSend;
+    uint32_t pwmDutyCycle = 1000;
 
-        printf2("VICIntEnable is: 0x%X\n\r", VICIntEnable);
-	
-	for(;;) {
-		//vSerialPutString(0, "Testing...\r\n", 50);
-		x++;
+    int myDataToSend;
 
-			FIO1CLR = (1<<19);//turn on led on olimex 2378 Sdev board
-//			FIO1SET = (1<<19);//turn off led on olimex 2378 dev board
-		if (x == interval) {
-//			FIO1SET = (1<<19);//turn on led on olimex 2378 dev board
-			
-			pwmDutyCycle += 100;
-			if(pwmDutyCycle > 2000 ) {
-				pwmDutyCycle = 1000;
-			}
-			setPWMDutyCycle(PWM1_1, microsecondsToCPUTicks(pwmDutyCycle));
-			
-		} else if (x >= (interval*2)) {
-//			FIO1CLR = (1<<19);//turn off led on olimex 2378 Sdev board
+    printf2("VICIntEnable is: 0x%X\n\r", VICIntEnable);
 
-			x = 0;
+    for(;;) {
+        //vSerialPutString(0, "Testing...\r\n", 50);
+        x++;
 
-                        // get id from blinkm and print
+        if (x == interval) {
+            FIO1SET = (1<<19);//turn on led on olimex 2378 dev board
 
-		       printf2("i2c Light Task...\r\n");
+            pwmDutyCycle += 100;
+            if(pwmDutyCycle > 2000 ) {
+                pwmDutyCycle = 1000;
+            }
+            setPWMDutyCycle(PWM1_1, microsecondsToCPUTicks(pwmDutyCycle));
 
-                       myDataToSend = 'c';
-                       printf2("VICRawIntr register is: 0x%X\n\r",VICRawIntr);
+        } else if (x >= (interval*2)) {
+            FIO1CLR = (1<<19);//turn off led on olimex 2378 Sdev board
 
-		       printf2("sending c...\r\n");
+            x = 0;
 
-                       I2C0MasterTX(BLINKM_ADDR, &myDataToSend, 1);
-                       printf2("VICRawIntr register is: 0x%X\n\r",VICRawIntr);
+            printf2("i2c Light Task...\r\n");
 
-                       myDataToSend = 0xff;
+            myDataToSend = 'c';
+            printf2("VICRawIntr register is: 0x%X\n\r",VICRawIntr);
 
-                       I2C0MasterTX(BLINKM_ADDR, &myDataToSend, 1);
+            printf2("sending c...\r\n");
 
-                       myDataToSend = 0xc4;
+            I2C0MasterTX(BLINKM_ADDR, &myDataToSend, 1);
 
-                       I2C0MasterTX(BLINKM_ADDR, &myDataToSend, 1);
+            printf2("VICRawIntr register is: 0x%X\n\r",VICRawIntr);
 
-                       status = xSerialGetChar(0, &theChar, 1);
-                       if( status == pdTRUE ) {
-                           printf2("You typed the character: '%c'\r\n", theChar);
-                       }
-                }
+            myDataToSend = 0xff;
+
+            I2C0MasterTX(BLINKM_ADDR, &myDataToSend, 1);
+
+            myDataToSend = 0xc4;
+
+            I2C0MasterTX(BLINKM_ADDR, &myDataToSend, 1);
+
+            status = xSerialGetChar(0, &theChar, 1);
+            if( status == pdTRUE ) {
+                printf2("You typed the character: '%c'\r\n", theChar);
+            }
         }
+    }
 }
 
 
@@ -190,59 +181,59 @@ static void i2cblinkmTask(void *pvParameters) {
 
 static void prvSetupHardware( void )
 {
-	#ifdef RUN_FROM_RAM
-		/* Remap the interrupt vectors to RAM if we are are running from RAM. */
-		SCB_MEMMAP = 2;
-	#endif
-	
-	/* Disable the PLL. */
-	PLLCON = 0;
-	PLLFEED = mainPLL_FEED_BYTE1;
-	PLLFEED = mainPLL_FEED_BYTE2;
-	
-	/* Configure clock source. */
-	SCS |= mainOSC_ENABLE;
-	while( !( SCS & mainOSC_STAT ) );
-	CLKSRCSEL = mainOSC_SELECT; 
-	
-	/* Setup the PLL to multiply the XTAL input by 4. */
-	PLLCFG = ( mainPLL_MUL | mainPLL_DIV );
-	PLLFEED = mainPLL_FEED_BYTE1;
-	PLLFEED = mainPLL_FEED_BYTE2;
+#ifdef RUN_FROM_RAM
+    /* Remap the interrupt vectors to RAM if we are are running from RAM. */
+    SCB_MEMMAP = 2;
+#endif
 
-	/* Turn on and wait for the PLL to lock... */
-	PLLCON = mainPLL_ENABLE;
-	PLLFEED = mainPLL_FEED_BYTE1;
-	PLLFEED = mainPLL_FEED_BYTE2;
-	CCLKCFG = mainCPU_CLK_DIV;	
-	while( !( PLLSTAT & mainPLL_LOCK ) );
-	
-	/* Connecting the clock. */
-	PLLCON = mainPLL_CONNECT;
-	PLLFEED = mainPLL_FEED_BYTE1;
-	PLLFEED = mainPLL_FEED_BYTE2;
-	while( !( PLLSTAT & mainPLL_CONNECTED ) ); 
+    /* Disable the PLL. */
+    PLLCON = 0;
+    PLLFEED = mainPLL_FEED_BYTE1;
+    PLLFEED = mainPLL_FEED_BYTE2;
 
-	
-	/* 
-	This code is commented out as the MAM does not work on the original revision
-	LPC2368 chips.  If using Rev B chips then you can increase the speed though
-	the use of the MAM.
-	
-	Setup and turn on the MAM.  Three cycle access is used due to the fast
-	PLL used.  It is possible faster overall performance could be obtained by
-	tuning the MAM and PLL settings.
-	MAMCR = 0;
-	MAMTIM = mainMAM_TIM_3;
-	MAMCR = mainMAM_MODE_FULL;
-	*/
+    /* Configure clock source. */
+    SCS |= mainOSC_ENABLE;
+    while( !( SCS & mainOSC_STAT ) );
+    CLKSRCSEL = mainOSC_SELECT; 
+
+    /* Setup the PLL to multiply the XTAL input by 4. */
+    PLLCFG = ( mainPLL_MUL | mainPLL_DIV );
+    PLLFEED = mainPLL_FEED_BYTE1;
+    PLLFEED = mainPLL_FEED_BYTE2;
+
+    /* Turn on and wait for the PLL to lock... */
+    PLLCON = mainPLL_ENABLE;
+    PLLFEED = mainPLL_FEED_BYTE1;
+    PLLFEED = mainPLL_FEED_BYTE2;
+    CCLKCFG = mainCPU_CLK_DIV;	
+    while( !( PLLSTAT & mainPLL_LOCK ) );
+
+    /* Connecting the clock. */
+    PLLCON = mainPLL_CONNECT;
+    PLLFEED = mainPLL_FEED_BYTE1;
+    PLLFEED = mainPLL_FEED_BYTE2;
+    while( !( PLLSTAT & mainPLL_CONNECTED ) ); 
+
+
+    /* 
+       This code is commented out as the MAM does not work on the original revision
+       LPC2368 chips.  If using Rev B chips then you can increase the speed though
+       the use of the MAM.
+
+       Setup and turn on the MAM.  Three cycle access is used due to the fast
+       PLL used.  It is possible faster overall performance could be obtained by
+       tuning the MAM and PLL settings.
+       MAMCR = 0;
+       MAMTIM = mainMAM_TIM_3;
+       MAMCR = mainMAM_MODE_FULL;
+       */
 }
 
 
 void enableSerial0( void ) {
-	PCLKSEL0 = (PCLKSEL0 & ~(3<<6)) | (1<<6);//set uart to run at CCLK speed, UART0
-	PINSEL0 |= mainTX_ENABLE;
-	PINSEL0 |= mainRX_ENABLE;
+    PCLKSEL0 = (PCLKSEL0 & ~(3<<6)) | (1<<6);//set uart to run at CCLK speed, UART0
+    PINSEL0 |= mainTX_ENABLE;
+    PINSEL0 |= mainRX_ENABLE;
 }
 
 
@@ -267,8 +258,6 @@ int main( void )
     // Initialize I2C0
     I2Cinit(I2C0);
 
-    printf2("B4 Task create: 0x%X\n\r",VICRawIntr);
-    printf2("I2C0CONSET B4: 0x%X\n\r",I2C0CONSET);
     xTaskCreate( i2cblinkmTask, 
             ( signed portCHAR * ) "i2cblinkmTask", 
             I2CTEST_STACK_SIZE, NULL, 
@@ -287,50 +276,50 @@ int main( void )
 
 void vApplicationTickHook( void )
 {
-	unsigned portBASE_TYPE uxColumn = 0;
-	static xLCDMessage xMessage = { 0, "PASS" };
-	static unsigned portLONG ulTicksSinceLastDisplay = 0;
-	static portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+    unsigned portBASE_TYPE uxColumn = 0;
+    static xLCDMessage xMessage = { 0, "PASS" };
+    static unsigned portLONG ulTicksSinceLastDisplay = 0;
+    static portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
 
-	// Called from every tick interrupt.  Have enough ticks passed to make it
-        // time to perform our health status check again? 
-	ulTicksSinceLastDisplay++;
-	if( ulTicksSinceLastDisplay >= mainCHECK_DELAY )
-	{
-		ulTicksSinceLastDisplay = 0;
-		
-		/* Has an error been found in any task? */
+    // Called from every tick interrupt.  Have enough ticks passed to make it
+    // time to perform our health status check again? 
+    ulTicksSinceLastDisplay++;
+    if( ulTicksSinceLastDisplay >= mainCHECK_DELAY )
+    {
+        ulTicksSinceLastDisplay = 0;
+
+        /* Has an error been found in any task? */
 
         if( xAreBlockingQueuesStillRunning() != pdTRUE )
-		{
-			xMessage.pcMessage = "ERROR - BLOCKQ";
-		}
+        {
+            xMessage.pcMessage = "ERROR - BLOCKQ";
+        }
 
-		if( xAreBlockTimeTestTasksStillRunning() != pdTRUE )
-		{
-			xMessage.pcMessage = "ERROR - BLOCKTIM";
-		}
+        if( xAreBlockTimeTestTasksStillRunning() != pdTRUE )
+        {
+            xMessage.pcMessage = "ERROR - BLOCKTIM";
+        }
 
-		if( xAreGenericQueueTasksStillRunning() != pdTRUE )
-		{
-			xMessage.pcMessage = "ERROR - GENQ";
-		}
-		
-		if( xAreQueuePeekTasksStillRunning() != pdTRUE )
-		{
-			xMessage.pcMessage = "ERROR - PEEKQ";
-		}       
-		
-		if( xAreDynamicPriorityTasksStillRunning() != pdTRUE )
-		{
-			xMessage.pcMessage = "ERROR - DYNAMIC";
-		}
-        
+        if( xAreGenericQueueTasksStillRunning() != pdTRUE )
+        {
+            xMessage.pcMessage = "ERROR - GENQ";
+        }
+
+        if( xAreQueuePeekTasksStillRunning() != pdTRUE )
+        {
+            xMessage.pcMessage = "ERROR - PEEKQ";
+        }       
+
+        if( xAreDynamicPriorityTasksStillRunning() != pdTRUE )
+        {
+            xMessage.pcMessage = "ERROR - DYNAMIC";
+        }
+
         xMessage.xColumn++;
 
-		/* Send the message to the LCD gatekeeper for display. */
-		xHigherPriorityTaskWoken = pdFALSE;
-	}
+        /* Send the message to the LCD gatekeeper for display. */
+        xHigherPriorityTaskWoken = pdFALSE;
+    }
 }
 
 
