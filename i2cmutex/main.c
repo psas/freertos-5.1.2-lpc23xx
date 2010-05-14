a3c34ea9e813a58f39f05f987d4c9e38b44deeab
davec <dave.camarillo@gmail.com>
2010-05-14 10:13:20 -0700

    fixed naming on millisecond conversion function

--- i2cmutex/main.c
+++ i2cmutex/main.c
@@ -92,7 +92,7 @@ uint32_t microsecondsToCPUTicks(const uint32_t microseconds) {
 /*
  * millisecondsToCPUTicks
  */
-uint32_t milisecondsToCPUTicks(const uint32_t miliseconds) {
+uint32_t millisecondsToCPUTicks(const uint32_t miliseconds) {
     uint32_t ret = (configCPU_CLOCK_HZ / 1000) * miliseconds;
     return(ret);
 }
